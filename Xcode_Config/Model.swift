8108911eb2dfadc9901c50a7749d73e7a32a02bc
Sarah Reichelt <sarahr@me.com>
2024-03-05 14:58:31 +1000

    Initial Commit

---
+++ Xcode_Config/Model.swift
@@ -0,0 +1,4 @@
+// Created on 4/3/2024.
+// Copyright (c) 2024 Sarah Reichelt, TrozWare.
+
+import Foundation
---
+++ Xcode_Config/Xcode_ConfigApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_ConfigApp.swift
+//  Xcode_Config
+//
+//  Created by Sarah Reichelt on 29/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_ConfigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
